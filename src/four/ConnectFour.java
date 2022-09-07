e5a9981a03a259b3864c44f4c459d7d27a5172a8
Denis Stoyanouski <vandenst@tut.by>
2022-09-07 20:19:28 +0300

    add method fillingBoard and fillHelper

--- src/four/ConnectFour.java
+++ src/four/ConnectFour.java
@@ -2,13 +2,15 @@ package four;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 
-public class ConnectFour extends JFrame implements ActionListener {
 
-    private int clickCounter = 0;
+public class ConnectFour extends JFrame {
+
+    static ArrayList<JButton> buttons = new ArrayList<>();
+    final int LENGTH = 7;
+    static final int HEIGHT = 6;
     public ConnectFour() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 400);
@@ -16,41 +18,20 @@ public class ConnectFour extends JFrame implements ActionListener {
         setVisible(true);
         setTitle("Connect Four");
 
-        Engine newArray = new Engine();
-        for (String[] row : newArray.fillGrid()) {
-            for(String column : row) {
+        for (int i = HEIGHT; i >= 1; i--) {
+            for (int j = 0; j < LENGTH; j++) {
+                char ch = (char) ('A' + j);
+                String name = String.format("Button%c%d", ch, i);
                 JButton button = new JButton(" ");
-                button.setName("Button" + column);
+                button.setName(name);
                 button.setFocusPainted(false);
-                button.addActionListener(this);
+                button.addActionListener(actionEvent -> Engine.fillingBoard(ch));
                 add(button);
-
-                /*JLabel label = new JLabel(" ");
-                label.setHorizontalAlignment(SwingConstants.CENTER);
-                label.setVerticalAlignment(SwingConstants.CENTER);
-                label.setName(column);
-                label.setFont(new Font("Courier", Font.BOLD,12));
-                label.setFont(label.getFont().deriveFont(15f));
-                label.setVisible(true);
-                button.add(label);*/
-
+                buttons.add(button);
             }
         }
         setLayout(new GridLayout(6, 7, 1, 1));
         setVisible(true);
-
-    }
-    public void actionPerformed(ActionEvent e) {
-        JButton change = (JButton) e.getSource();
-        if (clickCounter % 2 != 0) {
-            change.setText("O");
-        } else {
-            change.setText("X");
-        }
-        clickCounter++;
-
-
-
     }
 
 }
--- src/four/Engine.java
+++ src/four/Engine.java
@@ -1,17 +1,51 @@
 package four;
 
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Engine {
 
-    protected String[][] fillGrid() {
-        char letter = 'A';
-        int number = 6;
-        String[][] grid = new String[6][7];
-            for(int row = 0; row < grid.length; row++) {
-                for(int column = 0; column < grid[0].length; column++ ) {
-                   grid[row][column] = String.valueOf((char) (letter + column)).concat(String.valueOf(number - row));
+
+    private static Map<Character, Integer> filled = new HashMap<>();
+    private static boolean firstPlayer = true;
+
+
+     static void fillingBoard(char ch) {
+        if (!filled.containsKey(ch)) {
+            filled.put(ch, 1);
+            JButton button = null;
+            for (JButton b : ConnectFour.buttons) {
+                if (b.getName().equals(String.format("Button%c1", ch))) {
+                    button = b;
+                    break;
                 }
             }
-        return grid;
+            fillHelper(button);
+            return;
+        }
+        if (filled.get(ch) == ConnectFour.HEIGHT) {
+            return;
+        }
+        JButton button = null;
+        int index = filled.get(ch);
+        for (JButton b: ConnectFour.buttons) {
+            if (b.getName().equals(String.format("Button%c%d", ch, index + 1))) {
+                button = b;
+                break;
+            }
+        }
+        fillHelper(button);
+        filled.put(ch, index + 1);
+    }
+    private static void fillHelper(JButton button) {
+        assert button.getText().equals(" ");
+        if (firstPlayer) {
+            button.setText("X");
+        } else {
+            button.setText("O");
+        }
+        firstPlayer = !firstPlayer;
     }
 
 
