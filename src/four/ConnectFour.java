749ce2d4ed83f38aa5157761ce27cffd619f3c07
Denis Stoyanouski <vandenst@tut.by>
2022-09-07 16:16:02 +0300

    add actionPerformed method for changing button text to X or O;

--- src/four/ConnectFour.java
+++ src/four/ConnectFour.java
@@ -2,8 +2,13 @@ package four;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ConnectFour extends JFrame {
+
+public class ConnectFour extends JFrame implements ActionListener {
+
+    private int clickCounter = 0;
     public ConnectFour() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 400);
@@ -11,32 +16,40 @@ public class ConnectFour extends JFrame {
         setVisible(true);
         setTitle("Connect Four");
 
-
-
         Engine newArray = new Engine();
         for (String[] row : newArray.fillGrid()) {
             for(String column : row) {
-                JButton button = new JButton(column);
+                JButton button = new JButton(" ");
                 button.setName("Button" + column);
+                button.addActionListener(this);
                 add(button);
-                /*JLabel label = new JLabel(column);
+
+                /*JLabel label = new JLabel(" ");
                 label.setHorizontalAlignment(SwingConstants.CENTER);
                 label.setVerticalAlignment(SwingConstants.CENTER);
+                label.setName(column);
                 label.setFont(new Font("Courier", Font.BOLD,12));
                 label.setFont(label.getFont().deriveFont(15f));
-
+                label.setVisible(true);
                 button.add(label);*/
+
             }
         }
         setLayout(new GridLayout(6, 7, 1, 1));
         setVisible(true);
 
-
-
+    }
+    public void actionPerformed(ActionEvent e) {
+        JButton change = (JButton) e.getSource();
+        if (clickCounter % 2 != 0) {
+            change.setText("O");
+        } else {
+            change.setText("X");
+        }
+        clickCounter++;
 
 
 
     }
-
 
 }
