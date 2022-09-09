c3c0e0c606c6f17158e1c6fd5abc26e61c855e77
Denis Stoyanouski <vandenst@tut.by>
2022-09-09 21:30:14 +0300

    added method createDiagonalFromLeftToRight and createDiagonalFromRightToLeft

--- src/four/Engine.java
+++ src/four/Engine.java
@@ -60,9 +60,72 @@ public class Engine {
         }
         hasLineFour(horizontalLine, letter);
         hasLineFour(verticalLine, letter);
-
+        hasLineFour(createDiagonalFromLeftToRight(generatedKey), letter);
+        hasLineFour(createDiagonalFromRightToLeft(generatedKey), letter);
 
     }
+
+    static ArrayList<String> createDiagonalFromLeftToRight(String generatedKey) {
+
+         char letterOfKey = generatedKey.charAt(0);
+         char numberOfKey = generatedKey.charAt(1);
+         while (letterOfKey >= 'A' || numberOfKey >= '1') {
+             if (letterOfKey == 'A' || numberOfKey == '1') {
+                 break;
+             }
+             letterOfKey--;
+             numberOfKey--;
+         }
+         System.out.println(letterOfKey + "" + numberOfKey);
+
+         ArrayList<String> diagonalLiftRight = new ArrayList<>();
+         char startNumber = numberOfKey;
+         char startLetter = letterOfKey;
+
+         while (true) {
+             String key = startLetter + "" + startNumber;
+             diagonalLiftRight.add(key);
+             startLetter++;
+             startNumber++;
+             if (startNumber > '6' || startLetter > 'G') {
+                 break;
+             }
+         }
+         System.out.println(diagonalLiftRight);
+         return diagonalLiftRight;
+    }
+
+    static ArrayList<String> createDiagonalFromRightToLeft(String generatedKey) {
+
+        char letterOfKey = generatedKey.charAt(0);
+        char numberOfKey = generatedKey.charAt(1);
+        while (numberOfKey <= '6' || letterOfKey >= 'A') {
+            if(letterOfKey == 'A' || numberOfKey == '6') {
+                break;
+            }
+            letterOfKey--;
+            numberOfKey++;
+        }
+
+         ArrayList<String> diagonalLiftRight = new ArrayList<>();
+        char startLetter = letterOfKey;
+        char startNumber = numberOfKey;
+
+        while (startNumber >= '1' || startLetter <= 'G') {
+            String key = startLetter + "" + startNumber;
+            diagonalLiftRight.add(key);
+            startLetter++;
+            startNumber--;
+            if (startLetter > 'G' || startNumber < '1') {
+                break;
+            }
+        }
+
+        System.out.println(diagonalLiftRight);
+        return diagonalLiftRight;
+    }
+
+
 
     static void hasLineFour(ArrayList<String> line, String letter) {
          String checker = letter.repeat(4);
